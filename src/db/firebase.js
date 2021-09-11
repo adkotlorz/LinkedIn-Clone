c016628b1570192997fc9042178dd6ef27d7c9ba
Adam <ad.kotlorz@gmail.com>
2021-09-11 23:52:17 +0200

    cleanup

---
+++ src/db/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCLIixKxisHs6yEFLjn0tgZSgxCoLSb0Wc",
+    authDomain: "linkedin-clone-ce1af.firebaseapp.com",
+    projectId: "linkedin-clone-ce1af",
+    storageBucket: "linkedin-clone-ce1af.appspot.com",
+    messagingSenderId: "960430401544",
+    appId: "1:960430401544:web:e257b27cb113c256ed09bd",
+    measurementId: "G-49SJ0F0GKJ"
+};
